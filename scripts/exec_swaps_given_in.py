807262fdf483a40b0c8add687d0fb0c9a91f6701
Alessio Delmonti <alessio.d@gmail.com>
2022-07-25 18:54:31 +0100

    Output of swaps in console

--- scripts/exec_swaps_given_in.py
+++ scripts/exec_swaps_given_in.py
@@ -1,3 +1,4 @@
+import json
 from ape_safe import ApeSafe
 from brownie import ZERO_ADDRESS
 from eth_abi import encode_single
@@ -8,12 +9,14 @@ HALF_HOUR = 1800
 WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
 DEV_SAFE_ADDRESS = "0x6458A23B020f489651f2777Bd849ddEd34DfCcd2"
 
-MIGRATOR = ""
+MIGRATOR = "0x7940c0225c836742e8953F69E0af0C4b371a35BF"
 ROUTER_UNIV2 = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
 ROUTER_SUSHI = "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F"
 ROUTER_UNIV3 = "0xE592427A0AEce92De3Edee1F18E0157C05861564"
 QUOTER_UNIV3 = "0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6"
 FACTORY_UNIV3 = "0x1F98431c8aD98523631AE4a59f267346ea31F984"
+
+FACTORY_ABI = json.loads('[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"uint24","name":"fee","type":"uint24"},{"indexed":true,"internalType":"int24","name":"tickSpacing","type":"int24"}],"name":"FeeAmountEnabled","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"oldOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnerChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"token0","type":"address"},{"indexed":true,"internalType":"address","name":"token1","type":"address"},{"indexed":true,"internalType":"uint24","name":"fee","type":"uint24"},{"indexed":false,"internalType":"int24","name":"tickSpacing","type":"int24"},{"indexed":false,"internalType":"address","name":"pool","type":"address"}],"name":"PoolCreated","type":"event"},{"inputs":[{"internalType":"address","name":"tokenA","type":"address"},{"internalType":"address","name":"tokenB","type":"address"},{"internalType":"uint24","name":"fee","type":"uint24"}],"name":"createPool","outputs":[{"internalType":"address","name":"pool","type":"address"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint24","name":"fee","type":"uint24"},{"internalType":"int24","name":"tickSpacing","type":"int24"}],"name":"enableFeeAmount","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint24","name":"","type":"uint24"}],"name":"feeAmountTickSpacing","outputs":[{"internalType":"int24","name":"","type":"int24"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"address","name":"","type":"address"},{"internalType":"uint24","name":"","type":"uint24"}],"name":"getPool","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"parameters","outputs":[{"internalType":"address","name":"factory","type":"address"},{"internalType":"address","name":"token0","type":"address"},{"internalType":"address","name":"token1","type":"address"},{"internalType":"uint24","name":"fee","type":"uint24"},{"internalType":"int24","name":"tickSpacing","type":"int24"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_owner","type":"address"}],"name":"setOwner","outputs":[],"stateMutability":"nonpayable","type":"function"}]')
 
 
 def quote_univ2(router, token_in, token_out, amount_in):
@@ -37,7 +40,7 @@ def swap_univ2(router, token_in, token_out, max_in, amount_out, account):
 
     interface.ERC20(token_in).approve(router, max_in, {"from": account})
 
-    router.swapTokensForExactTokens(
+    router.swapTokensForExactTokens.encode_input(
         amount_out,
         max_in,
         [token_in, token_out],
@@ -59,6 +62,7 @@ def quote_univ3(factory, quoter, token_in, token_out, amount_in):
             quote = quoter.quoteExactInputSingle.call(
                 token_in, token_out, 3000, amount_in, 0
             )
+
             return quote
         except:
             return 0
@@ -94,19 +98,22 @@ def swap_univ3(router, token_in, token_out, max_in, amount_out, account):
             amount_out,
             max_in,
             0,
-        ],
-        {"from": account},
+        ]
     )
 
 
 def main():
     safe = ApeSafe(DEV_SAFE_ADDRESS)
-    migrator = BasketMigrator(MIGRATOR)
+    migrator = safe.contract(MIGRATOR)
+
+    # migrator.burnAndUnwrap({"from": DEV_SAFE_ADDRESS})
     router_univ2 = Contract.from_explorer(ROUTER_UNIV2)
     router_univ3 = Contract.from_explorer(ROUTER_UNIV3)
     router_sushi = Contract.from_explorer(ROUTER_SUSHI)
     quoter_univ3 = Contract.from_explorer(QUOTER_UNIV3)
-    factory_univ3 = Contract.from_explorer(FACTORY_UNIV3)
+    factory_univ3 = Contract.from_abi("Factory", FACTORY_UNIV3, FACTORY_ABI)
+
+    
 
     bdi_assets = [
         "0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e",
@@ -128,34 +135,57 @@ def main():
 
     # exec bdi swaps to eth
     swaps = []
+    index = 0 
     for t in bdi_assets:
         token = interface.ERC20(t)
-        bal = token.balanceOf(migrator)
+        bal = token.balanceOf("0x0309c98b1bffa350bcb3f9fb9780970ca32a5060")
         univ2_out = quote_univ2(router_univ2, t, WETH, bal)
         sushi_out = quote_univ2(router_sushi, t, WETH, bal)
         univ3_out = quote_univ3(factory_univ3, quoter_univ3, t, WETH, bal)
 
+
+        print("------------------------------------SWAP ", index, "------------------------------------")
         if univ2_out >= sushi_out and univ2_out >= univ3_out:
+            print("Is v3? False")
+            print(router_univ2.address, [t, WETH], bal, univ2_out)
+            print(encode_single("(address,address[],uint256,uint256)",[router_univ2.address, [t, WETH], bal, univ2_out],).hex())
             swaps.append(
                 (
                     False,
                     encode_single(
                         "(address,address[],uint256,uint256)",
                         [router_univ2.address, [t, WETH], bal, univ2_out],
-                    ),
+                    ).hex(),
                 )
             )
         elif sushi_out >= univ2_out and sushi_out >= univ3_out:
+            print("Is v3? False")
+            print(router_sushi.address, [t, WETH], bal, sushi_out)
+            print(encode_single("(address,address[],uint256,uint256)",[router_sushi.address, [t, WETH], bal, sushi_out]).hex())
             swaps.append(
                 (
                     False,
                     encode_single(
                         "(address,address[],uint256,uint256)",
                         [router_sushi.address, [t, WETH], bal, sushi_out],
-                    ),
+                    ).hex(),
                 )
             )
         else:
+            print("Is v3? True")
+            print(router_univ3.address, t, WETH, 3000, bal, univ3_out)
+            print(encode_single(
+                        "(address,address,address,uint24,uint256,uint256)",
+                        [
+                            router_univ3.address,
+                            t,
+                            WETH,
+                            3000,
+                            bal,
+                            univ3_out,
+                        ],
+                    ).hex()
+                )
             swaps.append(
                 (
                     True,
@@ -169,19 +199,21 @@ def main():
                             bal,
                             univ3_out,
                         ],
-                    ),
+                    ).hex(),
                 )
             )
+    print("\n\n")
+    index += 1
 
-    migrator.execSwaps(swaps, chain.time() + HALF_HOUR, {"from": safe})
+    # migrator.execSwaps(swaps, chain.time() + HALF_HOUR, {"from": DEV_SAFE_ADDRESS})
 
-    weth_erc20 = interface.ERC20(WETH)
-    weth_balance_migrator = weth_erc20.balanceOf(DEV_SAFE_ADDRESS)
+    # weth_erc20 = interface.ERC20(WETH)
+    # weth_balance_migrator = weth_erc20.balanceOf(DEV_SAFE_ADDRESS)
 
-    print(f"weth balance of migrator: {weth_balance_migrator / 1e18}")
+    # print(f"weth balance of migrator: {weth_balance_migrator / 1e18}")
 
-    safe_tx = safe.multisend_from_receipts()
+    # safe_tx = safe.multisend_from_receipts()
 
-    safe.preview(safe_tx)
-    safe.sign_with_frame(safe_tx)
-    safe.post_transaction(safe_tx)
+    # safe.preview(safe_tx)
+    # safe.sign_with_frame(safe_tx)
+    # safe.post_transaction(safe_tx)
